f5786a5d6cdaba4f614a13ff3ee59e9faafd08db
Alessandro Re <ale@ale-re.net>
2020-05-12 17:54:33 +0200

    Starting to have a texture

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,38 @@ use winit::{
     window::Window,
 };
 
+// Structure for a frame
+struct BgrFrame {
+    height: usize,
+    width: usize,
+    data: Vec<u8>,
+}
+
+
+fn create_pattern(size: usize) -> Vec<u8> {
+    use std::iter;
+
+    (0..size * size)
+        .flat_map(|id| {
+            // get high five for recognizing this ;) -> it's mandelbrot
+            let cx = 3.0 * (id % size) as f32 / (size - 1) as f32 - 2.0;
+            let cy = 2.0 * (id / size) as f32 / (size - 1) as f32 - 1.0;
+            let (mut x, mut y, mut count) = (cx, cy, 0);
+            while count < 0xFF && x * x + y * y < 4.0 { // squared sum and escape control
+                let old_x = x;
+                x = x * x - y * y + cx;
+                y = 2.0 * old_x * y + cy;
+                count += 1;
+            }
+            iter::once(0xFF - (count * 5) as u8)
+                .chain(iter::once(0xFF - (count * 15) as u8))
+                .chain(iter::once(0xFF - (count * 50) as u8))
+                .chain(iter::once(1))
+        })
+        .collect()
+}
+
+
 async fn run(evl: EventLoop<()>, win: Window) {
     let size = win.inner_size();
     /* from master branch
@@ -38,16 +70,94 @@ async fn run(evl: EventLoop<()>, win: Window) {
     let fs_module = device.create_shader_module(&wgpu::read_spirv(std::io::Cursor::new(&fs[..])).unwrap());
 
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        bindings: &[],
-        label: None,
-    });
-    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        layout: &bind_group_layout,
-        bindings: &[],
+        bindings: &[
+            wgpu::BindGroupLayoutEntry { // Accessing texture in frag
+                binding: 0,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::SampledTexture {
+                    multisampled: false,
+                    component_type: wgpu::TextureComponentType::Float,
+                    dimension: wgpu::TextureViewDimension::D2,
+                },
+            },
+            wgpu::BindGroupLayoutEntry { // Sampling texture in frag
+                binding: 1,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::Sampler { comparison: false },
+            },
+        ],
         label: None,
     });
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        bind_group_layouts: &[],
+        bind_group_layouts: &[&bind_group_layout],
+    });
+
+    // Init encoder is used to write to texture
+    let mut init_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    // Create the pattern
+    let image_data = create_pattern(256);
+    // Create texture of required size
+    let texture_extent = wgpu::Extent3d {
+        width: 256,
+        height: 256,
+        depth: 1,
+    };
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+        label: None,
+        size: texture_extent,
+        array_layer_count: 1,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+    });
+    let texture_view = texture.create_default_view();
+    // Copy texture 
+    let temp_buff = device.create_buffer_with_data(image_data.as_slice(), wgpu::BufferUsage::COPY_SRC);
+    init_encoder.copy_buffer_to_texture(
+        wgpu::BufferCopyView {
+            buffer: &temp_buff,
+            offset: 0,
+            bytes_per_row: 4 * 256,
+            rows_per_image: 0,
+        },
+        wgpu::TextureCopyView {
+            texture: &texture,
+            mip_level: 0,
+            array_layer: 0,
+            origin: wgpu::Origin3d::ZERO,
+        },
+        texture_extent,
+    );
+
+    let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        address_mode_w: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Nearest,
+        min_filter: wgpu::FilterMode::Linear,
+        mipmap_filter: wgpu::FilterMode::Nearest,
+        lod_min_clamp: -100.0,
+        lod_max_clamp: 100.0,
+        compare: wgpu::CompareFunction::Undefined,
+    });
+
+    // Create bind group to access texture
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &bind_group_layout,
+        bindings: &[
+            wgpu::Binding {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&texture_view),
+            },
+            wgpu::Binding {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(&sampler),
+            },
+        ],
+        label: None,
     });
 
     let swapchain_format = wgpu::TextureFormat::Bgra8UnormSrgb;
--- src/shader.vert
+++ src/shader.vert
@@ -4,18 +4,37 @@ out gl_PerVertex {
     vec4 gl_Position;
 };
 
+layout(location = 0) out vec2 v_TexCoord;
+
 void main() {
     float d = 0.9;
     /*
     vec2 position;
     switch (gl_VertexIndex) {
-        case 0: position = vec2(-d, -d); break;
-        case 1: position = vec2(+d, -d); break;
-        case 2: position = vec2(-d, +d); break;
-        case 3: position = vec2(+d, +d); break;
+        case 0: {
+            v_TexCoord = vec2(0, 0);
+            gl_Position = vec4(-1.0, -1.0, 0.0, 1.0);
+            break;
+        }
+        case 1: {
+            v_TexCoord = vec2(1, 0);
+            gl_Position = vec4(+1.0, -1.0, 0.0, 1.0);
+            break;
+        }
+        case 2: {
+            v_TexCoord = vec2(0, 1);
+            gl_Position = vec4(-1.0, +1.0, 0.0, 1.0);
+            break;
+        }
+        case 3: {
+            v_TexCoord = vec2(1, 1);
+            gl_Position = vec4(+1.0, +1.0, 0.0, 1.0);
+            break;
+        }
         default: position = vec2(0.5, 0.5); break;
     }
     */
-    vec2 position = 2.0 * d * vec2(gl_VertexIndex & 1, gl_VertexIndex / 2) - d;
-    gl_Position = vec4(position, 0.0, 1.0);
+    vec2 position = vec2(gl_VertexIndex & 1, gl_VertexIndex / 2);
+    v_TexCoord = position;
+    gl_Position = vec4(2.0 * d * position - d, 0.0, 1.0);
 }
